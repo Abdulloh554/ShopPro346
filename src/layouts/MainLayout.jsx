931f9987a02ec6e1bcef1aa78bf83e8db3d14eb2
Abdulloh554 <gofurjonovabdulloh551@gmail.com>
2026-03-16 05:54:57 +0500

    creat header creat mainLayout in children creat for children files

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,29 +1,33 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { FaCartPlus } from "react-icons/fa6";
 import { IoHeartCircle } from "react-icons/io5";
+import Logo from "../assets/vite.png";
 
 function MainLayout() {
   return (
     <>
       <header>
-        <div>
+        <div className="flex justify-between px-[80px] py-[30px] shadow-xl ">
           <div>
-            <NavLink to="/">Logo</NavLink>
+            <NavLink to="/home"><img src={Logo} alt="" /></NavLink>
           </div>
-          <nav>
-            <NavLink to="">Contact</NavLink>
-            <NavLink to="">About Us</NavLink>
-            <NavLink to="">
-              <IoHeartCircle style={{ fontSize: "40px", color: "blue" }} />
+          <nav className="flex gap-[30px]">
+            <NavLink className="hover:text-indigo-400 duration-300 ease-out font-bold " to="/home">Home</NavLink>
+            <NavLink className="hover:text-indigo-400 duration-300 ease-out font-bold " to="/contact">Contact</NavLink>
+            <NavLink className="hover:text-indigo-400 duration-300 ease-out font-bold " to="/aboutus">About Us</NavLink>
+            <NavLink to="/like" className="mt-[-10px]">
+              <IoHeartCircle style={{ fontSize: "40px", color: "red" }} />
             </NavLink>
-            <NavLink to="">
-              <FaCartPlus style={{ fontSize: "40px", color: "red" }} />
+            <NavLink to="/card" className="mt-[-10px]" >
+              <FaCartPlus style={{ fontSize: "40px", color: "black" }} />
             </NavLink>
-            <NavLink to="">Login</NavLink>
+            <NavLink className="hover:text-indigo-400 duration-300 ease-out font-bold" to="/login">Login</NavLink>
           </nav>
         </div>
       </header>
+      <main>
+        <Outlet/>
+      </main>
     </>
   );
 }
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,11 +3,43 @@ import AdminLayout from "../layouts/AdminLayout";
 import SellerLayout from "../layouts/SellerLayout";
 import MainLayout from "../layouts/MainLayout";
 import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import AboutUs from "../pages/AboutUs";
+import Like from "../pages/Like";
+import Card from "../pages/Card";
+import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    children:[
+      {
+        path:"/home",
+        element:<Home/>,
+      },
+      {
+        path:"/contact",
+        element:<Contact/>,
+      },
+      {
+        path:"/aboutus",
+        element:<AboutUs/>,
+      },
+      {
+        path:"/like",
+        element:<Like/>
+      },
+      {
+        path:"/card",
+        element:<Card/>
+      },
+      {
+        path:"/login",
+        element:<Login/>
+      },
+    ]
   },
   {
     path: "/admin",
